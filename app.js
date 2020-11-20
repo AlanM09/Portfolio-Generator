f0862f4edbd2ff12336296798a3e0513036db251
AlanM09 <71355872+AlanM09@users.noreply.github.com>
2020-11-20 14:53:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ console.log(profileDataArgs);
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
 const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i++) {
+  for (let i = 0; i < profileDataArr.length; i += 1) {
     console.log(profileDataArr[i]);
   }
+  console.log('================');
+  
+   // Is the same as this...
+  profileDataArr.forEach((profileItem) => {
+    console.log(profileItem)
+  });
 };
