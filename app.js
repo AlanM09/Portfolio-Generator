36aa2cb9d439744d6b2901af9a59d02eecdd7c2f
AlanM09 <71355872+AlanM09@users.noreply.github.com>
2020-11-20 15:43:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-const generatePage = () => 'Name: Jane, Github: janehub';
-console.log(generatePage());
+const generatePage = (username, githubName) => `Name: ${userName}, Github: ${githubName}`;
+console.log(generatePage('Jane', 'janehub'));
